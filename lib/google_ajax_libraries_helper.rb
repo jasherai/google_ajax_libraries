25f47756e22993ff4031cf8a29378cf1b6e388db
Francesc Esplugas <fesplugas@intraducibles.net>
2008-05-28 14:19:05 +0200

    Ideas around this plugin ...

--- lib/google_ajax_libraries_helper.rb
+++ lib/google_ajax_libraries_helper.rb
@@ -2,13 +2,14 @@ module GoogleAjaxLibraries
 
   module Helper
 
-    def google_javascript_include_tag(ajax_library, options = {})
-      <<-HTML
-        <script src="http://www.google.com/jsapi"></script>
-        <script>
-          google.load("#{ajax_library}", "#{options[:version] || 1 }", {uncompressed:#{options[:uncompressed] || false }});
-        </script>
-      HTML
+    def google_javascript_include_tag(ajax_libraries)
+      html = "<script src=\"http://www.google.com/jsapi\"></script>\n"
+      html << "<script>\n"
+      ajax_libraries.each do |k, v|
+        html << "  google.load(\"#{k}\", \"#{v[:version] || 1 }\", {uncompressed:#{v[:uncompressed] || false }});\n"
+      end
+      html << "</script>"
+      return html
     end
 
   end
