b407334e6d24c2b6b5c32a0250e82a32b97e8467
Francesc Esplugas <fesplugas@intraducibles.net>
2008-05-29 22:05:59 +0200

    Now works as I wanted to ...

--- lib/google_ajax_libraries_helper.rb
+++ lib/google_ajax_libraries_helper.rb
@@ -2,11 +2,15 @@ module GoogleAjaxLibraries
 
   module Helper
 
-    def google_javascript_include_tag(ajax_libraries)
+    def google_javascript_include_tag(*ajax_libraries)
       html = "<script src=\"http://www.google.com/jsapi\"></script>\n"
       html << "<script>\n"
-      ajax_libraries.each do |k, v|
-        html << "  google.load(\"#{k}\", \"#{v[:version] || 1 }\", {uncompressed:#{v[:uncompressed] || false }});\n"
+      ajax_libraries.each do |al|
+        begin
+          html << "  google.load(\"#{al[0]}\", \"#{al[1] || 1 }\", {uncompressed:#{al[2] || false }});\n"
+        rescue
+          html << "  google.load(\"#{al}\", \"1\");\n"
+        end
       end
       html << "</script>"
       return html
