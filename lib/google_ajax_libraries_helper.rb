3d257c3996d660133cc4dee5d77d730edcd0d3b3
Pritesh Mehta <pritesh@phatforge.com>
2008-10-07 13:47:03 +0100

    Ok so the extension didn't work as a module. :( Poor testing on my part.
    Fixed module code
    
    Bugs: Can't specify lone library with version.

--- lib/google_ajax_libraries_helper.rb
+++ lib/google_ajax_libraries_helper.rb
@@ -2,22 +2,23 @@ module GoogleAjaxLibraries
 
   module Helper
 
-    def google_javascript_include_tag(scripts={}, opts={}, *backward_compat)
+    def google_javascript_include_tag(scripts={}, opts={})
       key = opts[:key] || nil
-      ajax_libraries = *scripts
+      ajax_libraries = *scripts.to_a
       html = ""
-      
+      debug = false
       if debug
        html << "<pre>Options = #{opts.to_json}</pre>\n"
        html << "<pre>key = #{key.to_json}</pre>\n"
        html << "<pre>Lib to load = #{ajax_libraries.to_json}</pre>\n"
+       html << "<pre>Lib = #{ajax_libraries.to_a.each { |l| puts l } }</pre>\n"
       end
       
       html << "<script type=\"text/javascript\" src=\"http://www.google.com/jsapi"
       html << "?key=#{key}" if key
       html << "\"></script>\n"
       html << "<script>\n"
-      ajax_libraries.each do |al|
+      ajax_libraries.to_a.each do |al|
         begin
           html << "  google.load(\"#{al[0]}\", \"#{al[1] || 1 }\", {uncompressed:#{al[2] || false }});\n"
         rescue
